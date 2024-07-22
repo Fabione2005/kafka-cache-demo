456354ab87461894d71a5b50a18ed106f506d600
fabio <fabione2005@gmail.com>
2024-07-22 16:59:57 -0400

    Primet commit de archivos iniciales

---
+++ src/main/java/net/fabione/springboot_kafka_cache_circuitbraker/SpringbootKafkaCacheCircuitbrakerApplication.java
@@ -0,0 +1,13 @@
+package net.fabione.springboot_kafka_cache_circuitbraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaCacheCircuitbrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaCacheCircuitbrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fabione/springboot_kafka_cache_circuitbraker/SpringbootKafkaCacheCircuitbrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fabione.springboot_kafka_cache_circuitbraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaCacheCircuitbrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
